d3f0e35592e46879d312d1d82f4b30c1e2ffb768
Antoine <antoine.labarussias@epita.fr>
2021-11-05 14:26:18 +0100

    feat(ref): add task2

--- .ref/task1.tf
+++ .ref/task1.tf
@@ -1,4 +1,4 @@
 resource "taikun_kubernetes_profile" "tfws_k8s_profile" {
-  name = "tfws_k8s_profile"
+  name            = "tfws_k8s_profile"
   organization_id = resource.taikun_organization.tfws_organization.id
 }
---
+++ .ref/task3.tf
@@ -0,0 +1,16 @@
+resource "taikun_slack_configuration" "tfws_slack" {
+  name    = "tfws_slack"
+  channel = "ci"
+  url     = "https://hooks.myapp.example/ci"
+  type    = "Alert"
+
+  organization_id = resource.taikun_organization.tfws_organization.id
+}
+
+resource "taikun_alerting_profile" "tfws_alerting_profile" {
+  name     = "tfws_alerting_profile"
+  reminder = "None"
+
+  slack_configuration_id = resource.taikun_slack_configuration.tfws_slack.id
+  organization_id        = resource.taikun_organization.tfws_organization.id
+}
